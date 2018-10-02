90f7eb8812f5dba9e4bfc099501c273f141a0e96
AfaMadza <Afa.Madza@gmail.com>
2018-10-02 15:21:43 -0700

    Secured API Keys

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import * as actionTypes from './actionTypes';
 import config from '../../config';
 
+//Hide API_KEYS
+
 let API_KEY = config.API_KEY;
 
 export const authStart = () => {
