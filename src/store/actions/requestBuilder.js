2c64b1691de4047c42a2bd5a2a5be1ec378afcb6
AfaMadza <Afa.Madza@gmail.com>
2018-10-02 18:25:58 -0700

    Implemented Modal for Request Summary and refactored requestBuilder reducer

--- src/store/actions/requestBuilder.js
+++ src/store/actions/requestBuilder.js
@@ -1,9 +1,9 @@
 import * as actionTypes from './actionTypes';
 //import axios from '../../axios-orders';
 
-export const setRequest = ( orderData ) => {
+export const setRequest = ( order ) => {
     return {
         type: actionTypes.SET_REQUEST,
-        orderData: orderData
+        order: order
     };
 };
